5ac93a20c4ed9c33dea1cd8311385f897cf8028c
Viktor Sonesten <vikson-6@student.ltu.se>
2020-09-05 01:03:10 +0200

    sd-card.nix -> mmc-image.nix, improve build.sh

--- build.sh
+++ build.sh
@@ -7,9 +7,9 @@ set -eou pipefail
 #[[ ! -w ${1} ]] && echo "Target ${1} is not writable. Do you have permissions?" && exit 2
 
 echo "Building image..."
-image=$(nixos-generate -f sd-aarch64-installer --system aarch64-linux -c sd-card.nix)
-echo "Flashing target..."
-sudo dd if=${image} of=${1} bs=64k status=progress
+image=$(nixos-generate -f sd-aarch64-installer --system aarch64-linux -c mmc-image.nix)
+echo "Flashing ${1}..."
+sudo dd if=${image} of=${1} bs=64k conv=fdatasync
 
 # TODO: check UID of SD card at start of script, ensure it is the same when starting dd.
 # TODO: how do we build this remotely?
