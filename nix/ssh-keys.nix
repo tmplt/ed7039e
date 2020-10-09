3ab26cd52c97eb216e1f0ac96843e63278c1c0b3
lllalex <70566527+lllalex@users.noreply.github.com>
2020-10-09 14:57:26 +0200

    nix: add SSH key (#27)

--- nix/ssh-keys.nix
+++ nix/ssh-keys.nix
@@ -1,4 +1,5 @@
 {
   tmplt = "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBBrsSG9or4lj7j/aBwzIYX98m8kHD78ulKxhNnThWkInoK8iDGdrBzBkLdXRDneTxDWpqZEQBUyIAX1ZTkvluNU= tmplt@perscitia-2019-11-13";
   simpen = "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBNmYY8kvkqscucREcHBnUZr+3k3icyO8SVjL4Div2WZ2XnvxHoJgj4DT06qTgSEzed8l4iewr7xAg9gk4BFqd9w= ecdsa-key-20201008";
+  Alex = "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBBrH7WTfiI2x048XQkEjk7x6RsFo0PYk+C81okp2zOsHavTL+RHkziBrgsK5A9gLJWClMvY7pvhs7lkSUxMtfqI= ecdsa-key-20201009";
 }
