0fbacbb93e43edf5dda455c2840e8c10771791db
Viktor Sonesten <vikson-6@student.ltu.se>
2020-10-30 03:03:26 +0100

    refactor(dwm): poll position using apg instead of tlv

--- src/lcm-source-dwm.c
+++ src/lcm-source-dwm.c
@@ -154,66 +154,6 @@ int read_until(int fd, char *str, char *buf)
         return rdlen;
 }
 
-/* Calls the remote command `fun`, checks function validity, reads function return
- * payload into the buffer starting at `buf` and return payload length.
- */
-int tlv_rpc(int fd, char fun, char *buf, char *respbuf)
-{
-        /* Call function. */
-        char format[] = "tlv %02x 00\r";
-        char cmd[sizeof("tlv 02 00\r")];
-        snprintf(cmd, sizeof(cmd), format, fun);
-
-        memset(buf, 0, BUFFER_SIZE);
-        memset(respbuf, 0, BUFFER_SIZE);
-
-        /* The interactive shell echoes back written bytes,
-         * which it expects us to read before processing next incoming bytes.
-         */
-        int retval = 0;
-        for (size_t i = 0; i < sizeof(cmd); i++) {
-                /* char b;         /\* XXX: required instead of buf: lest "OUTPUT FRAME" is shredded on consequent calls. Why? *\/ */
-                if (write(fd, cmd + i, 1) < 1 ||
-                    (retval = readt(fd, buf + i, 1)) < 1) {
-                        printf("tlv_rpc: could not call function: %s\n", strerror(errno));
-                        return -1;
-                }
-        }
-
-        /* Read out full response. */
-        if ((retval = read_until(fd, "\r\ndwm> ", buf)) < 0) {
-                printf("tlv_rpc: could not read out full response: %s\n", strerror(errno));
-                return -1;
-        }
-
-        if (!strstr(buf, "OUTPUT FRAME")) {
-                puts("tlv_rpc: missing expected response frame header");
-                return -1;
-        }
-
-        /* Assume we made a correct function call and find the prefix location. */
-        char ok_funcall_prefix[] = "40 01 00";
-        if ((buf = strstr(buf, ok_funcall_prefix)) == NULL) {
-                puts("tlv_rpc: did not find expected OK prefix in repsonse frame");
-                return -1;
-        }
-        buf += sizeof(ok_funcall_prefix);
-        
-        /* Convert the hexadecimal payload to binary in-place. */
-        int payload_hex_len = strstr(buf, "\r\ndwm> ") - buf;
-        if (payload_hex_len <= 0) {
-                puts("tlv_rpc: could not find response suffix");
-                return -1;
-        }
-        int i = 0;
-        for (; i * 3 < payload_hex_len; i++) {
-                /* Read the hexadecimal byte from the buffer and write it back in binary. */
-                sscanf(buf + (i * 3), "%02hhx", respbuf + i);
-        }
-
-        return i - TL_HEADER_LEN; /* return payload length */
-}
-
 void timespec_diff(struct timespec *a, struct timespec *b, struct timespec *r)
 {
         r->tv_sec = MAX(a->tv_sec - b->tv_sec, 0);
@@ -222,21 +162,31 @@ void timespec_diff(struct timespec *a, struct timespec *b, struct timespec *r)
 
 void poll_position_loop(ctx_t *ctx)
 {
+        int retval;
         robot_dwm_position_t pos;
         memset(&pos, 0, sizeof(pos));
-        char respbuf[BUFFER_SIZE];      /* XXX: required? */
-                
-        /* Query measured position. */
-        int tlv_len = 0;
-        int error = 0;
-        if ((tlv_len = tlv_rpc(ctx->fd, dwm_pos_get, ctx->buf, respbuf)) < 0) {
-                error = 1;
+
+        char cmd[] = "apg\r";
+
+        if (write(ctx->fd, cmd, sizeof(cmd)) < sizeof(cmd)) {
+                puts("failed to write");
+                return;
         }
-        if (respbuf[0] != 0x41 && !error) {
-                printf("read unexpected payload type %02x\n", respbuf[0]);
-                error = 1;
+
+        /* Read out full response. */
+        if ((retval = read_until(ctx->fd, "\r\ndwm> ", ctx->buf)) < 0) {
+                printf("could not read out full response: %s\n", strerror(errno));
+                return;
         }
-        if (error) {
+
+        char *buf;
+        if ((buf = strstr(ctx->buf, "apg:")) == NULL) {
+                puts("could not find substring \"apg:\"");
+                return;
+        }
+
+        if ((retval = sscanf(buf, "apg: x:%ld y:%ld z:%ld qf:%d\r\n", &pos.x, &pos.y, &pos.z, &pos.q)) < 4) {
+                printf("pos: failed to sscanf buffer: %d fields correctly read out\n", retval);
                 return;
         }
 
@@ -246,14 +196,6 @@ void poll_position_loop(ctx_t *ctx)
                 clock_gettime(CLOCK_REALTIME, &ts);
                 pos.timestamp = (ts.tv_sec * 1e3) + round(ts.tv_nsec / 1e3f);
         }
-                
-        /* Copy payload into struct.
-         * XXX: `robot_dwm_position_t` is padded with 3B,
-         * but the below works in this case.
-         * TODO: dont memcpy, assign each struct member instead (or memcpy to them)
-         */
-        assert(tlv_len == 13);
-        memcpy(&pos.x, respbuf + TL_HEADER_LEN, tlv_len);
 
         /* Publish payload on appropriate channel. */
         robot_dwm_position_t_publish(ctx->lcm, "POSITION", &pos);
