1488739028a4968be7687664e1fa6f3bd8e07e4e
rubenasplund <ruben.asplund@hotmail.com>
2020-12-08 15:40:19 +0100

    Updated provider

--- arrowhead-clients/provider.py
+++ arrowhead-clients/provider.py
@@ -1,56 +1,52 @@
 import sys
 from datetime import datetime
+import time
+from sshtunnel import SSHTunnelForwarder
 sys.path.insert(1, '/home/ruben/git/client-library-python')
 import arrowhead_client.api as ar
+import requests
 
+
+remote_host = '192.168.43.44'           # ip to the raspberry pi
+remote_port = 22                        # port for ssh
+local_host = '127.0.0.1'
+local_port = 5005
+
+# Creating ssh tunnel for the communication between robot and server
+server = SSHTunnelForwarder(
+   (remote_host, remote_port),
+   ssh_username='pi',
+   ssh_private_key='/home/ruben/.ssh/id_rsa.pub',
+   remote_bind_address=(local_host, local_port),
+   local_bind_address=(local_host, local_port),
+   )
+
+
+# Creating Provider app which will be running on the server along arrowhead core systems
 provider_app = ar.ArrowheadHttpClient(
         system_name='lcm_provider',
         address='127.0.0.1',
         port=5000,
-        keyfile='certificates/provider.key',
-        certfile='certificates/provider.crt',
+        keyfile='certificates/my_cloud/lcm_provider.key',
+        certfile='certificates/my_cloud/lcm_provider.crt',
 )
 
-action = ''
-log =''
+# A service pick_up which will notify the robot to pick up a piece
+@provider_app.provided_service('pick_up', 'pick_up', 'HTTPS-SECURE-JSON', 'GET', )
+def pick_up(request):
+    url = 'http://{}:{}/robot/pick_up'.format(local_host, local_port)
+    response = requests.get(url).json()
+    return response
 
-@provider_app.provided_service(
-        'post_action',
-        'post_action',
-        'HTTPS-SECURE-JSON',
-        'POST', )
-def post_action(request):
-    global action
-    action = request.json['action']
-    print ('Action updated to:', action)
-    return {"msg": "OK!"}
-
-@provider_app.provided_service(
-        'get_action',
-        'get_action',
-        'HTTPS-SECURE-JSON',
-        'GET', )
-def get_action(request):
-    return {"action": action}
-
-@provider_app.provided_service(
-        'post_log',
-        'post_log',
-        'HTTPS-SECURE-JSON',
-        'POST', )
-def post_log(request):
-    global log
-    log = request.json['log']
-    print ('Log Updated to:',log)
-    return {"msg": "OK!"}
-
-@provider_app.provided_service(
-        'get_log',
-        'get_log',
-        'HTTPS-SECURE-JSON',
-        'GET', )
-def get_log(request):
-    return {'log': log}
+# A service place which will notify the robot to place a piece
+@provider_app.provided_service('place', 'place', 'HTTPS-SECURE-JSON', 'GET', )
+def place(request):
+    url = 'http://{}:{}/robot/place'.format(local_host, local_port)
+    response = requests.get(url).json()
+    return response
 
 if __name__ == '__main__':
+    # Starting ssh tunnel
+    server.start()
+    # Running the provider app
     provider_app.run_forever()
