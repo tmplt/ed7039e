683e9eba640c1c740a611101ab79e7ec883de36e
rubenasplund <ruben.asplund@hotmail.com>
2020-12-08 15:39:45 +0100

    Updated consumer

--- arrowhead-clients/consumer.py
+++ arrowhead-clients/consumer.py
@@ -6,32 +6,27 @@ import arrowhead_client.api as ar
 
 urllib3.disable_warnings()
 
+# Creating the consumer_app
 consumer_app = ar.ArrowheadHttpClient(
         system_name='consumer',
         address='127.0.0.1',
         port=5001,
-        keyfile='certificates/consumer.key',
-        certfile='certificates/consumer.crt',
+        keyfile='certificates/my_cloud/consumer.key',
+        certfile='certificates/my_cloud/consumer.crt',
 )
 
-consumer_app.add_consumed_service('post_action', 'POST')
-consumer_app.add_consumed_service('get_log', 'GET')
+# Add services to consume
+consumer_app.add_consumed_service('pick_up', 'GET')
+consumer_app.add_consumed_service('place', 'GET')
 
 if __name__ == '__main__':
-
-    response = consumer_app.consume_service('post_action', json={'action': 'pick_up_piece'})
+    
+    # Consume services and print out the response message
+    response = consumer_app.consume_service('pick_up')
     message = consumer_app.consumer.extract_payload(response, 'json')
     print (message['msg'])
-    time.sleep(3)
-    response = consumer_app.consume_service('post_action', json={'action': 'drop_off_piece'})
-    time.sleep(3)
-    response = consumer_app.consume_service('post_action', json={'action': ''})
-    time.sleep(3)
-    response = consumer_app.consume_service('post_action', json={'action': 'stop'})
-'''
-    for i in range(0,4):
-        response3 = consumer_app.consume_service('get_log')
-        message3 = consumer_app.consumer.extract_payload(response3, 'json')
-        print (message3['log'])
-        time.sleep(1)
-'''
+    time.sleep(2)
+
+    response2 = consumer_app.consume_service('place')
+    message2 = consumer_app.consumer.extract_payload(response2, 'json')
+    print (message2['msg'])
