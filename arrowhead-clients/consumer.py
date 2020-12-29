7a3001f773a736c6fab9db7c1c96c185fdc34f8d
rubenasplund <ruben.asplund@hotmail.com>
2020-12-29 12:10:55 +0100

    Changed to post request

--- arrowhead-clients/consumer.py
+++ arrowhead-clients/consumer.py
@@ -1,9 +1,5 @@
-''' This is a consumer which will ask the provider for services'''
-import time
 import urllib3
 import sys
-# Path to the client-library-python which the consumer is using
-# https://github.com/arrowhead-f/client-library-python
 sys.path.insert(1, '/home/ruben/git/client-library-python')
 import arrowhead_client.api as ar
 
@@ -14,22 +10,34 @@ consumer_app = ar.ArrowheadHttpClient(
         system_name='consumer',
         address='127.0.0.1',
         port=5001,
-        keyfile='certificates/my_cloud/consumer.key',
-        certfile='certificates/my_cloud/consumer.crt',
+        keyfile='certificates/consumer.key',
+        certfile='certificates/consumer.crt',
 )
 
 # Add services to consume
-consumer_app.add_consumed_service('pick_up', 'GET')
-consumer_app.add_consumed_service('place', 'GET')
+consumer_app.add_consumed_service('pick_up', 'POST')
+consumer_app.add_consumed_service('place', 'POST')
 
 if __name__ == '__main__':
     
-    # Consume services and print out the response message
-    response = consumer_app.consume_service('pick_up')
-    message = consumer_app.consumer.extract_payload(response, 'json')
-    print (message['msg'])
-    time.sleep(2)
-
-    response2 = consumer_app.consume_service('place')
-    message2 = consumer_app.consumer.extract_payload(response2, 'json')
-    print (message2['msg'])
+    while True:
+        # Consume services and print out the response message
+        print('Enter service:')
+        x = input()
+        if x == 'pick_up l' or x == 'pick up l':
+            response = consumer_app.consume_service('pick_up', json={'position': 'loading'})
+            message = consumer_app.consumer.extract_payload(response, 'json')
+            print (message['msg'])
+        if x == 'place l':
+            response2 = consumer_app.consume_service('place', json={'position': 'loading'})
+            message2 = consumer_app.consumer.extract_payload(response2, 'json')
+            print (message2['msg'])
+        if x == 'pick_up u' or x == 'pick up u':
+            response = consumer_app.consume_service('pick_up', json={'position': 'unloading'})
+            message = consumer_app.consumer.extract_payload(response, 'json')
+            print (message['msg'])
+        if x == 'place u':
+            response2 = consumer_app.consume_service('place', json={'position': 'unloading'})
+            message2 = consumer_app.consumer.extract_payload(response2, 'json')
+            print (message2['msg'])
+        
